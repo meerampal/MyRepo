589efafc92e0b9dc294bdddb784f5c22becdb261
meerampal <meerampal1991@gmail.com>
2019-10-07 12:06:49 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MobileTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+
+	
+	@Test
+	public void browserAutomationNew() 
+	{
+		System.out.println("This is sample mobile test1.");
+	}
+	
+	@Test
+	public void elementsUiNew()
+	{
+		System.out.println("This is sample mobile test2.");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation() 
+	{
+		System.out.println("This is sample test1.");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("This is sample test2.");
+	}
+	
+	
+}
